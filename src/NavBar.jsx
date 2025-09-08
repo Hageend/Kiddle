0cd75445f76067e7afb99a14526f0a9e917c3ce0
Nnncyyy <137011245+Nnncyyy@users.noreply.github.com>
2025-09-07 18:18:09 -0600

    files

---
+++ src/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="/assets/logo2.png" alt="Logo" />
+      </div>
+      
+    </nav>
+  );
+}
+
+export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,29 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import ReactDOM from "react-dom/client"
 import App from './App'
+import InicioSesion from "./InicioSesion"
+import RegistroCliente from "./RegistroCliente"
+import Carrito from "./Carrito"
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+/*ReactDOM.createRoot(document.getElementById('root')).render(<InicioSesion/>)
+ReactDOM.createRoot(document.getElementById('root')).render(<RegistroCliente/>)
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)*/
+
+function Main() {
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<App/>}/>
+                <Route path="/InicioSesion" element={<InicioSesion/>}/>
+                <Route path="/RegistroCliente" element={<RegistroCliente/>}/>
+                <Route path="/inicio-sesion" element={<InicioSesion />} />
+                <Route path="/registro-cliente" element={<RegistroCliente />} />
+                <Route path="/Carrito" element={<Carrito />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default Main;
+ReactDOM.createRoot(document.getElementById("root")).render(<Main />);
